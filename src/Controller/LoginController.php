17b91c1a3585f45f6cd17e5ea1cd83e1cd16ca45
Rija Lalaina <rijalaraz@gmail.com>
2024-08-19 22:38:04 +0300

    Login form, espace membre, logout

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Gérer les erreurs
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Dernier username (email)
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function someAction(Security $security): Response
+    {
+        // logout the user in on the current firewall
+        $response = $security->logout();
+
+        // you can also disable the csrf logout
+        $response = $security->logout(false);
+
+        // ... return $response (if set) or e.g. redirect to the homepage
+        return $response;
+    }
+}
