e73bd66ec11daf2f2857bfdf775eccf2d99487f9
Rija Lalaina <rijalaraz@gmail.com>
2024-08-27 00:46:31 +0300

    Creation of the product sheet view

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/product/{slug}', name: 'app_product')]
+    public function index($slug, ProductRepository $productRepository): Response
     {
+        $product = $productRepository->findOneBySlug($slug);
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'product' => $product,
         ]);
     }
 }
