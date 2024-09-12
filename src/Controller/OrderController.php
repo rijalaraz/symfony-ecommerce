5db4d6a054780e6d49eaca062571fe690cf6d7ad
Rija Lalaina <rijalaraz@gmail.com>
2024-09-13 01:14:57 +0300

    Tunnel d'achat - Stocker les informations de la commande en base

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,12 +3,17 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Order;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Address;
+use App\Entity\OrderDetail;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 
 class OrderController extends AbstractController
 {
@@ -47,7 +52,7 @@ class OrderController extends AbstractController
      * Préparation du paiement vers stripe
      */
     #[Route('/order/summary', name: 'app_order_summary')]
-    public function add(Request $request, Cart $cart): Response
+    public function add(Request $request, Cart $cart, EntityManagerInterface $entityManager): Response
     {
         if ($request->getMethod() != 'POST') {
             return $this->redirectToRoute('app_cart');
@@ -60,6 +65,8 @@ class OrderController extends AbstractController
 
         $addresses = $user->getAddresses();
 
+        $products = $cart->getCart();
+
         $form = $this->createForm(OrderType::class, null, [
             'addresses' => $addresses,
         ]);
@@ -67,12 +74,48 @@ class OrderController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
-            
+            // Stocker les infos dans la BDD
+
+            // Création de la chaîne adresse
+            /**
+             * @var Address $addressObj
+             */
+            $addressObj = $form->get('addresses')->getData();
+            $address = sprintf('%s %s', $addressObj->getFirstname(), $addressObj->getLastname()).'<br>';
+            $address .= $addressObj->getAddress().'<br>';
+            $address .= sprintf('%s %s', $addressObj->getPostal(), $addressObj->getCity()).'<br>';
+            $address .= $addressObj->getCountry().'<br>';
+            $address .= $addressObj->getPhone();
+
+            $order = new Order();
+            $order->setCreatedAt(new \DateTime());
+            $order->setState(1);
+            $order->setCarrierName($form->get('carriers')->getData()->getName());
+            $order->setCarrierPrice($form->get('carriers')->getData()->getPrice());
+            $order->setDelivery($address);
+
+            foreach ($products as $product) {
+                $orderDetail = new OrderDetail();
+                /**
+                 * @var Product $productObj
+                 */
+                $productObj = $product['object'];
+
+                $orderDetail->setProductName($productObj->getName());
+                $orderDetail->setProductIllustration($productObj->getIllustration());
+                $orderDetail->setProductPrice($productObj->getPrice());
+                $orderDetail->setProductTva($productObj->getTva());
+                $orderDetail->setProductQuantity($product['qty']);
+                $order->addOrderDetail($orderDetail);
+            }
+
+            $entityManager->persist($order);
+            $entityManager->flush();
         }
 
         return $this->render('order/summary.html.twig', [
             'choices' => $form->getData(),
-            'cart' => $cart->getCart(),
+            'cart' => $products,
             'totalWt' => $cart->getTotalWt()
         ]);
     }
